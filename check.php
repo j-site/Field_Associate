378821af73a9ee4f6865c674fa4b21016ba0cae1
j-site <89094282+j-site@users.noreply.github.com>
2021-10-03 10:26:30 +0900

    Create check.php

---
+++ check.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Harvard Blog</title>
+</head>
+<body>
+<?php
+    echo ”こんにちは”;
+        print $_post['nickname'];
+            print '様';
+
+?>
+
+
+</body>
+</html>
